f57817d99a5443305c48e1ecb683fbfba980f43e
Marcelo-Marcal <marcelo.a.marcal@hotmail.com>
2023-08-25 09:01:11 -0300

    :construction: Estrutura inicial do painel

--- src/controller/dimensaoPaginaController.js
+++ src/controller/dimensaoPaginaController.js
@@ -9,6 +9,11 @@ function ajustaTamanhoPalcoJogo() {
 ajustaTamanhoPalcoJogo();
 
 function posicaoRandonica() {
+  //remover elemento (caso exista)
+  if(document.getElementById("mosquito")) {
+    document.getElementById("mosquito").remove();
+  }
+
   var posicaoX = Math.floor(Math.random() * largura) - 90;
   var posicaoY = Math.floor(Math.random() * altura) - 90;
 
@@ -16,28 +21,39 @@ function posicaoRandonica() {
   posicaoY = posicaoY < 0 ? 0 : posicaoY;
 
   console.log(posicaoX, posicaoY);
-
+  //criar elemento html
   var mosquito = document.createElement("img");
   mosquito.src = "../img/mosquito.png";
-  mosquito.className = tamanhoAleatorio();
+  mosquito.className = tamanhoAleatorio() + ' ' + ladoAleatorio();
   mosquito.style.left = posicaoX + "px";
   mosquito.style.top = posicaoY + "px";
   mosquito.style.position = "absolute";
+  mosquito.id = "mosquito";
 
   document.body.appendChild(mosquito);
-  tamanhoAleatorio();
-  console.log(tamanhoAleatorio());
+  // tamanhoAleatorio();
+  // ladoAleatorio();
+  // console.log(ladoAleatorio());
 }
 
 function tamanhoAleatorio() {
   var classe = Math.floor(Math.random() * 3);
-  
   switch(classe) {
     case 0:
-      return "mosquito1"        
+      return "mosquito1"
     case 1:
-      return "mosquito2"        
+      return "mosquito2"
     case 2:
-      return "mosquito3"        
+      return "mosquito3"
+  }
+}
+
+function ladoAleatorio() {
+  var classe = Math.floor(Math.random() * 2);  
+  switch(classe) {
+    case 0:
+      return "ladoA"
+    case 1:
+      return "ladoB"
   }
 }
